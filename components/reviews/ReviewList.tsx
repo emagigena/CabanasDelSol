46523525f061b13142fe28f94635525e58b1b6ae
emanuel <emanuelgigena79@gmail.com>
2023-06-19 14:51:54 -0300

    fix changes in reviews

--- components/reviews/ReviewList.tsx
+++ components/reviews/ReviewList.tsx
@@ -1,26 +1,39 @@
 import React from "react";
 import reviews from "../../data/reviews.json";
-import { Comment, Icon } from "semantic-ui-react";
+import { Card, Icon } from "semantic-ui-react";
+import logoGoogle from "../../public/google.png";
+import Image from "next/image";
 
 const Review = ({ name, comment, stars, image }) => {
   return (
-    <div className="flex bg-gray-100 p-4 rounded-md mb-4">
-      <Comment size="massive" className="flex mr-4">
-        <img
-          src={image}
-          className="flex rounded"
-          style={{ width: "60px", height: "60px" }}
-        />
-        <Comment.Content className="ml-10">
-          <Comment.Author as="a" className="font-bold text-black">
-            <button>{name}</button>
-          </Comment.Author>
-          <Comment.Text>{comment}</Comment.Text>
+    <div className="p-4 rounded-md mb-4">
+      <Card style={{width: "320px", height: "270px" }}>
+        <Card.Content>
+          <div style={{ display: "flex", alignItems: "center", padding: "10px" }}>
+            <img
+              src={image}
+              width={50}
+              height={50}
+              className="rounded"
+              alt={""}
+            />
+            <Card.Header className="font-bold text-black flex items-center ml-4">
+              <button>{name}</button>
+              <Icon style={{ marginLeft: "20px" }}>
+                <Image src={logoGoogle} width={24} height={24} alt={""} />
+              </Icon>
+            </Card.Header>
+          </div>
+          <Card.Description style={{ maxHeight: "100px", overflow: "auto" }}>
+            {comment}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
           {Array.from({ length: stars }).map((_, index) => (
             <Icon key={index} name="star" className="link" color="yellow" />
           ))}
-        </Comment.Content>
-      </Comment>
+        </Card.Content>
+      </Card>
     </div>
   );
 };
@@ -35,15 +48,17 @@ const ReviewList = () => {
           </p>
         </div>
       </div>
-      {reviews.map((review, index) => (
-        <Review
-          key={index}
-          name={review.name}
-          comment={review.comment}
-          stars={review.stars}
-          image={review.image}
-        />
-      ))}
+      <div className="flex flex-wrap justify-center">
+        {reviews.map((review, index) => (
+          <Review
+            key={index}
+            name={review.name}
+            comment={review.comment}
+            stars={review.stars}
+            image={review.image}
+          />
+        ))}
+      </div>
     </div>
   );
 };
