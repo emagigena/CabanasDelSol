2dea10054ca1ba118c01fa617c292eacdf2de817
emanuel <emanuelgigena79@gmail.com>
2023-09-17 21:07:46 -0300

    fix changes in images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "previews.dropbox.com"],
+    domains: ["lh3.googleusercontent.com", "youtube.googleapis.com"],
   },
 };
