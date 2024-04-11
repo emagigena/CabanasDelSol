485cbfa0cdf5284379a97762a035462c22f20d4a
emanuel <emanuelgigena79@gmail.com>
2024-04-11 09:16:26 -0300

    commit con el video

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from "next/image";
-import img from '../../public/hero.jpeg'
 
 export default function Header({ scrollHandler }) {
   return (
@@ -9,15 +7,10 @@ export default function Header({ scrollHandler }) {
       <div className="mx-auto pt-9">
         <div className="relative shadow-xl sm:overflow-hidden">
           <div className="absolute inset-0">
-            <Image
-              priority
-              fill
-              className="h-full w-full object-cover"
-              src={img}
-              placeholder="blur"
-              alt="Cabañas del Sol"
-              sizes="(max-width: 768px) 100vw, 50vw"
-            />
+            <video autoPlay muted loop playsInline className="h-full w-full object-cover">
+              <source src="/videos/promocioncab.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             <div className="absolute inset-0 bg-orange-100 mix-blend-multiply" />
           </div>
           <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
@@ -25,7 +18,6 @@ export default function Header({ scrollHandler }) {
               <span className="block text-white">
                 Vení a disfrutar a nuestro complejo
               </span>
-              {/* <span className="block text-orange-500">Cabañas</span> */}
             </h1>
 
             <div className="mx-auto mt-10 max-w-xs sm:flex sm:max-w-none sm:justify-center">
@@ -33,7 +25,7 @@ export default function Header({ scrollHandler }) {
                 className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8"
                 onClick={scrollHandler}
               >
-                Ver Mas
+                Ver Más
               </button>
             </div>
           </div>
